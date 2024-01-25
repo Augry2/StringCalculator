cfedbf16127d74ce0bf4ebb403b1428ef8bc89bd
august <august.rydnell@gmail.com>
2024-01-25 10:33:31 +0100

    return sum of two numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
+    public int add(String numbers) {
+        // convert string s to numbers separated with comma
+        String[] numbersArray = numbers.split(",");
+
+        int sum = 0;
+        int num = 0;
+
+        for (String curEle : numbersArray) {
+            num = Integer.parseInt(curEle);
+            sum = sum + num;
+        }
+
+        return sum;
+    }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class StringCalculatorTest {
+
+    // STEP 1
+    @Test
+    @DisplayName("add method will return sum of two comma separated numbers")
+    void addMethodWillReturnSumOfTwoCommaSeparatedNumbers() {
+        StringCalculator calc = new StringCalculator();
+
+        int result = calc.add("1,2");
+
+        assertThat(result).isEqualTo(3);
+    }
+
+
+
+
+
 
 }
